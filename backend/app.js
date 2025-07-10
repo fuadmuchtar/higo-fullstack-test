1febfc1caabaf0dc240863408777f517a207b54c
Fuad Muchtar <tech.fuad99@gmail.com>
2025-07-10 18:33:43 +0700

    initial project

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+
+const router = require('./routers')
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(cors())
+
+app.use('/', router)
+
+
+module.exports = app
